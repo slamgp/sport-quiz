dce36db9283bb26a2e6f1174bb8f3d6ee850c4ec
Evgeny Panchenko <evgeny.panchenko@playtech.com>
2016-06-23 16:32:15 +0300

    add mp3 download

--- src/main/java/com/slamgp/games/sportquiz/service/rest/DownloadController.java
+++ src/main/java/com/slamgp/games/sportquiz/service/rest/DownloadController.java
@@ -1,13 +1,12 @@
 package com.slamgp.games.sportquiz.service.rest;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.OutputStream;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,45 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class DownloadController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public void  getFile(HttpServletResponse response) {
-       File file = new File("src/main/webapp/resources/music/fon.jpg");
-       String mimeType = "image/jpeg";
-       if(file.exists()) {
-           transferFile(response, file, mimeType);
-       } else  {
-           try {
-            file.createNewFile();
-            FileWriter fw = new FileWriter(file.getAbsoluteFile());
-            BufferedWriter bw = new BufferedWriter(fw);
-            bw.write("aaaaaaaaaaaaaaaaaaaaaaaaa");
-            bw.close();
-
-            transferFile(response, file, mimeType);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void  getFile(HttpServletRequest request, HttpServletResponse response) {
+       String  filePath = "/resources/music/fon.mp3";
+       try {
+           ServletContext context = request.getServletContext();
+           String appPath = context.getRealPath("");
+           String fullPath =  appPath + filePath;
+           File file = new File(fullPath);
+           FileInputStream inputStream = new FileInputStream(file);
+           byte[] targetArray = new byte[inputStream.available()];
+           inputStream.read(targetArray);
+           response.getOutputStream().write(targetArray);
+           inputStream.close();
+       } catch (IOException e) {
+           e.printStackTrace();
        }
     }
-
-    private void transferFile(HttpServletResponse response, File file, String mimeType) {
-        response.setContentType(mimeType);
-        response.setHeader("Content-Disposition", String.format("inline; filename=\"" + file.getName() +"\""));
-        response.setContentLength((int)file.length());
-        byte[] bedStr;
-        bedStr = "CREATED OUTPUT STREAM FAILD!".getBytes();
-        try {
-         InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
-        // FileCopyUtils.copy(inputStream, response.getOutputStream());
-         byte[] targetArray = new byte[inputStream.available()];
-         inputStream.read(targetArray);
-         response.getOutputStream().write(targetArray);
-        } catch (IOException e) {
-             try {
-                response.getOutputStream().write(bedStr);
-            } catch (IOException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-            }
-        }
-     }
 }
